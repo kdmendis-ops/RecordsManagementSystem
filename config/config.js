45ad1881b7ed24f9803bbc2f7b8d91c8ca910e08
kdmendis-ops <kdmendis@gmail.com>
2026-08-05 16:17:08 -0400

    final config

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   port: process.env.PORT || 5001,
-  mongoUri: process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/team-informatics',
+  mongoUri: process.env.MONGODB_URI || 'mongodb+srv://kdmendis_db_user:QJ7F3mv3zTpZgE2e@cluster27227.csahrv4.mongodb.net/?appName=Cluster27227',
   jwtSecret: process.env.JWT_SECRET || 'replace_this_in_your_env_file',
   jwtExpiresIn: process.env.JWT_EXPIRES_IN || '7d'
 };
